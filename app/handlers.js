a2ab48dfeb188bb646546d965b2f1105ad3e8f3b
Owen Mundy <omundy@gmail.com>
2023-03-23 12:58:45 -0400

    Fix fortune path

--- app/handlers.js
+++ app/handlers.js
@@ -1,7 +1,7 @@
 
 
 // custom modules
-const fortune = require('./app/fortune');
+const fortune = require('./fortune');
 
 exports.home = (req,res) => res.render('home');
 exports.about = (req,res) =>
